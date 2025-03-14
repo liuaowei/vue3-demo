4a74fb529473d8c6684fbb338d52e3b6aa1dae9e
liuaowei <liuaowei@int-yt.com>
2025-03-14 17:40:39 +0800

    修改系统名称

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,7 @@ const transpose = (array: any[][]): any[][] => {
 
 const tableData = ref([]);
 
+// 上传之前的回调
 const handleBeforeUpload = (file: File) => {
   const reader = new FileReader();
 
@@ -45,31 +46,21 @@ const handleBeforeUpload = (file: File) => {
 };
 
 // 全部解析
-const handleParseAll = (index: number) => {
-  if (tableData.value.length == 0) {
-    return;
-  }
-  let item: any = tableData.value[index];
-  handleParse(item.request, index).then((response: any) => {
-    tableData.value[index] = {
-      ...tableData.value,
-      r_json: response,
-      entity: response.entity,
-      summary: response.summary,
-      time: response.time,
-      telephone: response.telephone,
-      location: response.location,
-      demand: response.demand,
-      demand_analysis: response.demand_analysis,
-      assign_department: response.assign_department,
-      basis_of_assign: response.basis_of_assign,
-    };
-    index++;
-    handleParseAll(index);
-    if (index == tableData.value.length) {
-      return;
-    }
-  });
+const handleParseAll = async () => {
+  let message = [];
+  message = tableData.value;
+  try {
+    const response: any = await fetch("http://127.0.0.1:5000/api/batch_data", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ message }),
+    });
+    console.log("response:", response);
+    tableData.value = response.message;
+    console.log("tableData.value:", tableData.value);
+  } catch (error) {}
 };
 
 // 解析
@@ -91,15 +82,6 @@ const handleParse = async (request: string, index: number) => {
     tableData.value[index] = {
       request: request,
       r_json: data || {},
-      entity: data.entity || "",
-      summary: data.summary || "",
-      time: data.time || "",
-      telephone: data.telephone || "",
-      location: data.location || "",
-      demand: data.demand || "",
-      demand_analysis: data.demand_analysis || "",
-      assign_department: data.assign_department || "",
-      basis_of_assign: data.basis_of_assign || "",
     };
     console.log("tableData:", tableData.value);
     console.log("data:123", data.entity);
@@ -109,31 +91,87 @@ const handleParse = async (request: string, index: number) => {
 };
 
 // 导出结果
-const exportResult = () => {};
+const exportResult = () => {
+  if (tableData.value.length === 0) {
+    console.warn("没有数据可供导出");
+    return;
+  }
+
+  // 将 tableData 转换为二维数组
+  const data = [
+    [
+      "诉求文本",
+      "问题实体",
+      "问题摘要",
+      "反映时间",
+      "联系方式",
+      "发生地点",
+      "反映诉求",
+      "诉求分析",
+      "指派部门",
+      "指派依据",
+    ],
+    ...tableData.value.map((row) => [
+      row.request,
+      row.r_json.entity,
+      row.r_json.summary,
+      row.r_json.time,
+      row.r_json.telephone,
+      row.r_json.location,
+      row.r_json.demand,
+      row.r_json.demand_analysis,
+      row.r_json.assign_department,
+      row.r_json.basis_of_assign,
+    ]),
+  ];
+
+  // 创建工作簿
+  const worksheet = XLSX.utils.aoa_to_sheet(data);
+  const workbook = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
+
+  // 导出 Excel 文件
+  XLSX.writeFile(workbook, "result.xlsx");
+};
 
 onMounted(() => {});
 </script>
 
 <template>
   <div class="main">
-    <h1>12345问题解析</h1>
+    <h1>12345市长热线分析系统</h1>
     <el-card class="content" shadow="never">
       <template #header>
-        <el-upload
-          :multiple="false"
-          accept=".xlsx"
-          :before-upload="handleBeforeUpload"
-          :show-file-list="false"
-        >
-          <el-button type="primary">上传Excel表单</el-button>
-        </el-upload>
+        <div style="display: flex; align-items: center">
+          <el-upload
+            :multiple="false"
+            accept=".xlsx"
+            :before-upload="handleBeforeUpload"
+            :show-file-list="false"
+          >
+            <el-button type="primary">上传Excel表单</el-button>
+          </el-upload>
+          <el-button
+            style="margin-left: 10px"
+            type="primary"
+            @click="handleParseAll"
+          >
+            全部解析
+          </el-button>
+          <el-button
+            style="margin-left: 10px"
+            type="primary"
+            @click="exportResult"
+          >
+            导出结果
+          </el-button>
+          <el-button style="margin-left: 10px" type="primary">
+            切换模型
+          </el-button>
+        </div>
       </template>
 
       <template #default>
-        <el-button type="primary" @click="handleParseAll(0)">
-          全部解析
-        </el-button>
-        <el-button type="primary" @click="exportResult">导出结果</el-button>
         <el-table height="800" :data="tableData">
           <el-table-column
             prop="request"
@@ -143,46 +181,73 @@ onMounted(() => {});
           >
           </el-table-column>
           <el-table-column label="问题实体" prop="entity" show-overflow-tooltip>
+            <template #default="scope">
+              {{ scope.row.r_json.entity }}
+            </template>
           </el-table-column>
           <el-table-column
             label="问题摘要"
             prop="summary"
             show-overflow-tooltip
           >
+            <template #default="scope">
+              {{ scope.row.r_json.summary }}
+            </template>
           </el-table-column>
           <el-table-column label="反映时间" prop="time" show-overflow-tooltip>
+            <template #default="scope">
+              {{ scope.row.time }}
+            </template>
           </el-table-column>
           <el-table-column
             label="联系方式"
             prop="telephone"
             show-overflow-tooltip
           >
+            <template #default="scope">
+              {{ scope.row.r_json.telephone }}
+            </template>
           </el-table-column>
           <el-table-column
             label="发生地点"
             prop="location"
             show-overflow-tooltip
           >
+            <template #default="scope">
+              {{ scope.row.r_json.location }}
+            </template>
           </el-table-column>
           <el-table-column label="反映诉求" prop="demand" show-overflow-tooltip>
+            <template #default="scope">
+              {{ scope.row.r_json.demand }}
+            </template>
           </el-table-column>
           <el-table-column
             label="诉求分析"
             prop="demand_analysis"
             show-overflow-tooltip
           >
+            <template #default="scope">
+              {{ scope.row.r_json.demand_analysis }}
+            </template>
           </el-table-column>
           <el-table-column
             label="指派部门"
             prop="assign_department"
             show-overflow-tooltip
           >
+            <template #default="scope">
+              {{ scope.row.r_json.assign_department }}
+            </template>
           </el-table-column>
           <el-table-column
             label="指派依据"
             prop="basis_of_assign"
             show-overflow-tooltip
           >
+            <template #default="scope">
+              {{ scope.row.r_json.basis_of_assign }}
+            </template>
           </el-table-column>
 
           <el-table-column label="操作">
